0f3d919fc765a88d88e7c278fabc7eaa955d6755
Harshit Pradhan <hpradhan020@gmail.com>
2024-10-30 22:59:37 +0530

    feat: added getVideoById function

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,3 +1,4 @@
+import { isValidObjectId } from "mongoose";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -91,4 +92,21 @@ const publishVideo = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, video, "Video published successfully."));
 });
 
-export { getAllVideos, publishVideo };
+const getVideoById = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id!");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(400, "Video with this id doesn't exist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video fetched successfully!"));
+});
+
+export { getAllVideos, publishVideo, getVideoById };
