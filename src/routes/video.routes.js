57e4b07d0c1a946d24be7d0064ad9a54467c3555
Harshit Pradhan <hpradhan020@gmail.com>
2024-10-30 22:28:17 +0530

    feat: added getAllVideos & publishVideo route

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,8 +1,28 @@
 import { Router } from "express";
-import { getAllVideos } from "../controllers/video.controller.js";
+import { getAllVideos, publishVideo } from "../controllers/video.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
+// we have to verify jwt for all the routes in this file
+// so, we will use a middleware
+router.use(verifyJWT);
 
-router.route("/").get(getAllVideos);
+router
+  .route("/")
+  .get(getAllVideos)
+  .post(
+    upload.fields([
+      {
+        name: "videoFile",
+        maxCount: 1,
+      },
+      {
+        name: "thumbnail",
+        maxCount: 1,
+      },
+    ]),
+    publishVideo
+  );
 
 export default router;
