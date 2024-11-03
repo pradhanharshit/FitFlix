a7708d03ebcd10aee185305318778fa37461700f
Harshit Pradhan <hpradhan020@gmail.com>
2024-11-03 22:43:10 +0530

    feat: added channelsSubscribed & subscribers count functions

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Subscription } from "../models/subscription.model.js";
-import { isValidObjectId } from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
@@ -40,4 +40,82 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   }
 });
 
-export { toggleSubscription };
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id!");
+  }
+
+  const channel = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(`${channelId}`),
+      },
+    },
+  ]);
+
+  const subscriberCount = channel.length;
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        subscriberCount,
+        "Subscribers count fetched successfully"
+      )
+    );
+});
+
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id!");
+  }
+
+  const channel = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: new mongoose.Types.ObjectId(`${channelId}`),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "details",
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              avatar: 1,
+              username: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        details: {
+          $first: "$details",
+        },
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        channel,
+        "Successfully fetched list of subscribed channels"
+      )
+    );
+});
+
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
