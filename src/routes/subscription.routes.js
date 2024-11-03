0a912fd0fb5cafe81908b7d443c46b450679c75b
Harshit Pradhan <hpradhan020@gmail.com>
2024-11-03 22:44:23 +0530

    feat: added route to get channelsSubscribed list & subscribers count

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import {
+  toggleSubscription,
+  getUserChannelSubscribers,
+  getSubscribedChannels,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/c/:channelId").post(toggleSubscription);
+router
+  .route("/c/:channelId")
+  .post(toggleSubscription)
+  .get(getSubscribedChannels);
+
+router.route("/u/:channelId").get(getUserChannelSubscribers);
 
 export default router;
