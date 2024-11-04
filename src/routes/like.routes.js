2fe3ea7c51cf592ef57cd00658bf80227cd22023
Harshit Pradhan <hpradhan020@gmail.com>
2024-11-04 17:21:36 +0530

    added route to toggle comment like

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import {
+  toggleVideoLike,
+  toggleCommentLike,
+} from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/toggle/v/:videoId").post(toggleVideoLike);
+router.route("/toggle/c/:commentId").post(toggleCommentLike);
 
 export default router;
