c35abae6a1af435743babb7b45b91a30481c1f0d
Harshit Pradhan <hpradhan020@gmail.com>
2024-11-10 15:28:18 +0530

    feat: added playlist model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+import { Video } from "./video.model.js";
+import { User } from "./user.model.js";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Video,
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: User,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
