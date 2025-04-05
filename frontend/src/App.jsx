d178d115e9830e960ead1ad78cc294c947671f57
Harshit Pradhan <hpradhan020@gmail.com>
2025-04-06 01:26:40 +0530

    feat: added app structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/NavBar/NavBar.jsx";
+import SideBar from "./components/SideBar/SideBar.jsx";
+import { useSelector } from "react-redux";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { themeObject } = useSelector((state) => state.theme);
   return (
-    <>
+    <div className="App">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar />
+        <main className="main__container flex h-[90vh]">
+          <SideBar />
+          <div
+            className="main__pages overflow-auto"
+            style={{ backgroundColor: themeObject.primary }}
+          >
+            <Outlet />
+          </div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
