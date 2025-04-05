3597a95c07191934def0252fc9cba67ad3306dc7
Harshit Pradhan <hpradhan020@gmail.com>
2025-04-06 01:27:07 +0530

    feat: redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
-createRoot(document.getElementById('root')).render(
+import Router from "./router/Router.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  </StrictMode>
+);
